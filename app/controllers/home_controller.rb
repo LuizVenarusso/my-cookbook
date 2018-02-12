f6ee2689aa3b76c1a327be0c10abb769b7f5f5d6
Luiz Gustavo Venarusso <luiz-gustavo10@hotmail.com>
2018-02-12 15:14:47 -0200

    Usuário vê receitas mais favoritadas na home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.last(6)
     @cuisines = Cuisine.all
     @recipe_types = RecipeType.all
+
+    aux_favorite = Favorite.group(:recipe_id).count.sort_by { |_a, b| b }
+    @favorite_recipe = aux_favorite.reverse.first(3).to_h
+    @favorite = []
+    @favorite_recipe.each_entry do |a, _b|
+      @favorite << Recipe.find(a)
+    end
   end
 end
